ba6da52ebb53f3a96bfddeea254d89cf63ae80ff
Vladislav <vladislaw.sirotkin@yandex.ru>
2026-08-29 19:42:19 +0300

    Обработчик Internal server error

--- ewm-stats-service/src/main/java/ru/practicum/ewmstats/server/exception/ErrorHandler.java
+++ ewm-stats-service/src/main/java/ru/practicum/ewmstats/server/exception/ErrorHandler.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Slf4j
 public class ErrorHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequest(Exception exception) {
         log.warn("Bad request: {}", exception.getMessage(), exception);
@@ -20,6 +20,18 @@ public class ErrorHandler {
                 .status(HttpStatus.BAD_REQUEST.name())
                 .reason("Incorrectly made request.")
                 .message(exception.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleServerError(Exception exception) {
+        log.error("Unexpected server error", exception);
+        return ErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                .reason("Internal server error.")
+                .message("Internal server error")
                 .timestamp(LocalDateTime.now())
                 .build();
     }
