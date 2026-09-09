2947e63fc2152a3cc1c6e693f53ed7a66b95ae79
Vladislav <vladislaw.sirotkin@yandex.ru>
2026-09-09 10:35:47 +0300

    замена EntityGraph на Query в методах репозитория

--- ewm-main-service/src/main/java/ru/practicum/ewm/compilation/repository/CompilationRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/compilation/repository/CompilationRepository.java
@@ -1,23 +1,37 @@
 package ru.practicum.ewm.compilation.repository;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.ewm.compilation.model.Compilation;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
-    @EntityGraph(attributePaths = {"events", "events.category", "events.initiator"})
-    Optional<Compilation> findDetailedById(Long id);
+
+    @Query("""
+            SELECT DISTINCT c FROM Compilation c
+            LEFT JOIN FETCH c.events e
+            LEFT JOIN FETCH e.category
+            LEFT JOIN FETCH e.initiator
+            WHERE c.id IN :ids
+            """)
+    List<Compilation> findDetailedByIdIn(@Param("ids") List<Long> ids);
 
     List<Compilation> findByPinned(Boolean pinned, Pageable pageable);
 
     List<Compilation> findAllBy(Pageable pageable);
 
-    @EntityGraph(attributePaths = {"events", "events.category", "events.initiator"})
-    List<Compilation> findDetailedByIdIn(List<Long> ids);
+    @Query("""
+            SELECT DISTINCT c FROM Compilation c
+            LEFT JOIN FETCH c.events e
+            LEFT JOIN FETCH e.category
+            LEFT JOIN FETCH e.initiator
+            WHERE c.id = :id
+            """)
+    Optional<Compilation> findDetailedById(@Param("id") Long id);
 
     boolean existsByTitle(String title);
 
