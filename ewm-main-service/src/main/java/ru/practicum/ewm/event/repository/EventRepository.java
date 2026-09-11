2175070885eac903d7516bf70478ab39ebcdac1f
Nepovezlo <sasasuharevskij98@gmail.com>
2026-09-11 04:13:59 +0300

    Checkstyle

--- ewm-main-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ru.practicum.ewm.event.model.Event;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
